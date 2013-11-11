c9af10796a419cb76163d1b7649981d3cb2bc9e0
Jan Geselle <geselle.jan@googlemail.com>
2013-11-11 12:25:06 +0100

    Initial Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+1 Minute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0:00 h";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "+5 Minuten";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "+10 Minuten";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "+30 Minuten";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(336, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "+60 Minuten";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Minuten sind 0.00 Stunden";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 264);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "0:00 h";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Taskbar;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int day;
+        private int max;
+        private Form1 that;
+        private Stopwatch timer = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+            day = 8 * 60;
+            max = 100;
+            that = this;
+
+            ThumbnailToolBarButton oneButton = new ThumbnailToolBarButton(TimeTrack.Properties.Resources.one, "+1 Minute");
+            oneButton.Click += button1_Click;
+
+            ThumbnailToolBarButton fiveButton = new ThumbnailToolBarButton(TimeTrack.Properties.Resources.five, "+5 Minuten");
+            fiveButton.Click += button2_Click;
+
+            ThumbnailToolBarButton tenButton = new ThumbnailToolBarButton(TimeTrack.Properties.Resources.ten, "+10 Minuten");
+            tenButton.Click += button3_Click;
+
+            ThumbnailToolBarButton thirtyButton = new ThumbnailToolBarButton(TimeTrack.Properties.Resources.thirty, "+30 Minuten");
+            thirtyButton.Click += button4_Click;
+
+            ThumbnailToolBarButton sixtyButton = new ThumbnailToolBarButton(TimeTrack.Properties.Resources.sixty, "+60 Minuten");
+            sixtyButton.Click += button5_Click;
+
+            TaskbarManager.Instance.ThumbnailToolBars.AddButtons(this.Handle, oneButton, fiveButton, tenButton, thirtyButton, sixtyButton);
+        }
+
+        private void callbacks() {
+            if (day < 0)
+            {
+                day = 8 * 60;
+            }
+            label1.Text = labelText();
+            label1.Update();
+            that.Text = labelText();
+            double percentage = day / 4.8;
+            progressBar1.Value = 100 - Convert.ToInt32(percentage);
+            progressBar1.Update();
+            var prog = Microsoft.WindowsAPICodePack.Taskbar.TaskbarManager.Instance;
+            prog.SetProgressState(Microsoft.WindowsAPICodePack.Taskbar.TaskbarProgressBarState.Normal);
+            prog.SetProgressValue(100 - Convert.ToInt32(percentage), max);
+        }
+
+        private string labelText() {
+            double tempHours = ( 480 - day ) / 60;
+            int hours = Convert.ToInt32(Math.Floor(tempHours));
+            int minutes = ( 480 - day ) % 60;
+            if (minutes < 10)
+            {
+                return hours + ":0" + minutes + " h";
+            }
+            else
+            {
+                return hours + ":" + minutes + " h";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            day = day - 1;
+            callbacks();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            day = day - 5;
+            callbacks();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            day = day - 10;
+            callbacks();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            day = day - 30;
+            callbacks();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            day = day - 60;
+            callbacks();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            float hours;
+            try
+            {
+                if (int.Parse(textBox1.Text) < 0)
+                {
+                    textBox1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    textBox1.ForeColor = Color.Black;
+                    hours = float.Parse(textBox1.Text) / 60;
+                    label2.Text = "Minuten sind " + String.Format("{0:0.00}", hours) + " Stunden";
+                    label2.Update();
+                }
+            }
+            catch
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+
+    }
+}
